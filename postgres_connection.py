974b830785c4b3607a508a42e888cb9140afd5c1
Thomas <thomashalberg@gmail.com>
2017-05-30 17:57:33 +0200

    Tutorial part 2

---
+++ postgres_connection.py
@@ -0,0 +1,8 @@
+
+import pandas as pd
+from sqlalchemy import Table, MetaData, create_engine
+engine = create_engine("postgresql://postgres:dota2db@localhost/dota")
+
+df = pd.DataFrame({'hej':2,'esfs':234,'234':'SFD:D'}, index=['TÆST'])
+
+df.to_sql('test', engine)
